289467cbf4d2493d117b2c0a3f02fecdea02a8e7
0x400@EA <hbn9@qq.com>
2021-09-24 17:31:12 +0800

    format

--- header.php
+++ header.php
@@ -6,61 +6,58 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="renderer" content="webkit">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115734355-1"></script>
-  <script>
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-   gtag('js', new Date());
-   gtag('config', 'UA-115734355-1');
-  </script>
   <title><?php $this->archiveTitle(array(
-      'category' => _t('分类 %s 下的文章'),
-      'search'  => _t('包含关键字 %s 的文章'),
-      'tag'    => _t('标签 %s 下的文章'),
-      'author'  => _t('%s 发布的文章')
+      'category' => '分类 %s 下的文章',
+      'search' => '包含关键字 %s 的文章',
+      'tag' => '标签 %s 下的文章',
+      'author' => '%s 发布的文章'
     ), '', ' - '); ?><?php $this->options->title() ?></title>
   <!-- 使用url函数转换相关路径 -->
   <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
   <!-- 通过自有函数输出HTML头部信息 -->
   <?php $this->header(); ?>
   <style>
-    body:before{ background:url( <?php echo getBingImage() ?> ) center 0 no-repeat; background-size:cover;}
+    body:before {
+      background: url( <?php echo getBingImage() ?> ) center 0 no-repeat;
+      background-size: cover;
+    }
   </style>
 </head>
 <body>
 <header id="header">
   <div class="container">
-      <div class="site-name">
+    <div class="site-name">
       <?php if ($this->options->logoUrl): ?>
         <a id="logo" href="<?php $this->options->siteUrl(); ?>">
-          <img src="<?php $this->options->logoUrl() ?>" alt="<?php $this->options->title() ?>" />
+          <img src="<?php $this->options->logoUrl() ?>" alt="<?php $this->options->title() ?>"/>
         </a>
         <div><?php $this->options->title() ?></div>
       <?php else: ?>
         <a id="logo" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a>
-    	  <p class="description"><?php $this->options->description() ?></p>
+        <p class="description"><?php $this->options->description() ?></p>
       <?php endif; ?>
-      </div>
+    </div>
 
-      <div class="site-nav">
-        <nav id="nav-menu" class="clearfix">
-          <a <?php if($this->is('index')): ?>class="current"<?php endif; ?> href="<?php $this->options->siteUrl(); ?>"><?php if($this->is('index')){_e('博客');}else{_e('首页');} ?></a>
-          <a href="https://sxlf.org" title="伤心凉粉的微博">微博</a>
-         <?php 
-         $this->archiveTitle(array(
-      'category' => _t('%s'),
-      'search'  => _t('搜索[%s]的结果'),
-      'tag'    => _t('%s'),
-      'author'  => _t('%s')
-    ), '<a href="" class="current title">', '</a>');
-    ?>
-          <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
-          <?php while($pages->next()): ?>
-          <a<?php if($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
-          <?php endwhile; ?>
-        </nav>
-      </div>
+    <div class="site-nav">
+      <nav id="nav-menu" class="clearfix">
+        <a <?php if ($this->is('index')): ?>class="current"<?php endif; ?>
+           href="<?php $this->options->siteUrl(); ?>"><?php if ($this->is('index')) {echo '博客';} else { echo '首页';} ?></a>
+        <a href="https://sxlf.org" title="伤心凉粉的微博">微博</a>
+        <?php
+        $this->archiveTitle(array(
+          'category' => '%s',
+          'search' => '搜索[%s]的结果',
+          'tag' => '%s',
+          'author' => '%s'
+        ), '<a href="" class="current title">', '</a>');
+        ?>
+        <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+        <?php while ($pages->next()): ?>
+          <a<?php if ($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?>
+            href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
+        <?php endwhile; ?>
+      </nav>
+    </div>
   </div>
 </header><!-- end #header -->
 <div id="body">
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 参考 https://dashkit.goodthemes.co
- * 
+ *
  * @package Dashkit
  * @author 0x400
  * @version 1.0
@@ -9,25 +9,29 @@
  */
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
- $this->need('header.php');
- ?>
+$this->need('header.php');
+?>
 
 <div id="main" role="main">
-	<?php while($this->next()): ?>
-        <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-			<h2 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-			<ul class="post-meta">
-              <li><span class="title">发表于：</span><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
-              <li><span class="title">分类：</span><?php $this->category(','); ?></li>
-              <li><?php Views_Plugin::theViews('<span class="title">访问：</span>','&nbsp;次'); ?></li>
-			</ul>
-            <div class="post-content" itemprop="articleBody">
-    			<?php $this->content('- 阅读剩余部分 -'); ?>
-            </div>
-        </article>
-	<?php endwhile; ?>
+  <?php while ($this->next()): ?>
+    <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+      <h2 class="post-title" itemprop="name headline"><a itemprop="url"
+                                                         href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+      </h2>
+      <ul class="post-meta">
+        <li><span class="title">发表于：</span>
+          <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
+        </li>
+        <li><span class="title">分类：</span><?php $this->category(','); ?></li>
+        <li><?php Views_Plugin::theViews('<span class="title">访问：</span>', '&nbsp;次'); ?></li>
+      </ul>
+      <div class="post-content" itemprop="articleBody">
+        <?php $this->content('- 阅读剩余部分 -'); ?>
+      </div>
+    </article>
+  <?php endwhile; ?>
 
-    <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
+  <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
 </div><!-- end #main-->
 
 <?php $this->need('sidebar.php'); ?>
